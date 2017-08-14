2520e3674899da6b3bcfed88aff5a443ac13e702
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-08-14 06:23:24 -0400

    added icons to connect

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-$('.name').click(function(){
-        event.preventDefault();
-        $('body').animate({scrollTop:0}, 'slow');
-    }); 
+$('.name').click(function () {
+    event.preventDefault();
+    $('body').animate({ scrollTop: 0 }, 'slow');
+});
+
+
+$('.js-more-button').click(function() {
+    event.preventDefault();
+    $('#more').toggleClass('hidden');
+    $('.js-more-button').addClass('hidden');
+    $('.js-scroll-top').removeClass('hidden');
+    $('body').animate({ scrollTop: $(document).height() }, 'slow');
+})
+
+$('.js-scroll-top').on('click', function() {
+    event.preventDefault();
+    $('body').animate({ scrollTop: 0 }, 'slow');
+    $('#more').addClass('hidden');
+    $('.js-scroll-top').addClass('hidden');
+    //Adding setTimeout so that the more button does not show before scroll up
+    setTimeout(function(){
+        $('.js-more-button').removeClass('hidden');
+     }, 1000);
+});
